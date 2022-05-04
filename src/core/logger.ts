c1036ddbb79b6598c782c2d82017bafbcef4f0e4
gurjotmindgrep <gurjot@mindgrep.com>
2022-05-04 18:03:18 +0530

    Integrated express-pino-logger

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,8 +1,17 @@
-import { debug } from 'console';
+import pinoDebug from  'pino-debug'
 import Pino from 'pino'
 
 const logger: Pino.Logger = Pino({
-    name: 'GS-logger'
-  })
+  name: 'GS-logger'
+});
+
+pinoDebug(logger, {
+  auto: true, // default
+  map: {
+    'GS-server': 'info',
+    'express:router': 'debug',
+    '*': 'trace' // everything else - trace
+  }
+})
 
 export { logger };
--- src/http_listener.ts
+++ src/http_listener.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import expressPinoLogger from 'express-pino-logger';
 import { logger } from './core/logger';
 
+const loggerExpress = expressPinoLogger({
+    logger: logger,
+    autoLogging: true,
+  });
 
 const app:express.Express = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(loggerExpress);
 // app.get('/', function(req, res) {
 //     console.log('called endpoint')
 // });
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.listen(port);
 logger.info('Node + Express REST API skeleton server started on port: %s', port)
 
