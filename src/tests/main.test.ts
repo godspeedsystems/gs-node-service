5320c9bf1242230e408469a8bff1537a1eb938b2
gurjotmindgrep <gurjot@mindgrep.com>
2022-05-12 21:25:57 +0530

    refactoring of test scripts

---
+++ src/tests/main.test.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect, glob, path, fs, PlainObject, expectObj } from './common';
+import { fail } from 'assert';
+
+const filenames = fs.readdirSync(__dirname);
+filenames.forEach(file => {
+    if (file.endsWith('.js') || file.endsWith('.ts') ) {
+        if (!( file.includes('common.js') || file.includes(path.basename(__filename))) ) {
+            const fileAbsPath = path.join(__dirname,file)
+            const { testName, testCode } = require(`${fileAbsPath}`);
+            const fixDir = path.join(__dirname, 'fixtures', testName);
+
+            describe(testName, () => {
+                glob(fixDir + '/*.?(js|ts)', function (err:Error|null, res: string[]) {
+                    res.map((file:string) => {
+                        const testId = path.basename(file).replace(/\.(js|ts)/i, '');
+                        it(testId, async () => {
+                            try {
+                              const result = await testCode(fixDir,testId);
+                              expect(result).to.eql(expectObj[testName][testId]);
+                            } catch(error) {
+                              fail(<Error>error);
+                            }
+                        });
+                    });
+                });
+            });
+        }
+    }
+});
