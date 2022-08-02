eef4c3a5d2aa76cb98f98c7aa0c59b4b5d7e20de
gurjotmindgrep <gurjot@mindgrep.com>
2022-08-02 20:10:47 +0530

    fix: Added spanStatus for error cases

--- src/functions/com/gs/datastore.ts
+++ src/functions/com/gs/datastore.ts
@@ -1,8 +1,11 @@
 import {GSStatus} from '../../../core/interfaces';
 import { logger } from '../../../core/logger';
-import { trace } from "@opentelemetry/api";
+import { trace, Span, SpanStatusCode } from "@opentelemetry/api";
 import { datastoreCounter, datastoreHistogram } from '../../../telemetry/monitoring';
+import { PlainObject } from '../../../core/common';
+
 const tracer = trace.getTracer('name');
+let datastoreSpan: Span;
 
 /**
  * 
@@ -15,40 +18,64 @@ export default async function(args:{[key:string]:any;}) {
   logger.debug('args %o', args.data);
   
   const ds = args.datasource;
+  let prismaMethod: any; let status_message: string; 
+
   const [entityType, method] = args.config.method.split('.');
-  let prismaMethod = ds.client[entityType][method];
 
   // Start a datastore span
-  const datastoreSpan = tracer.startSpan(`datastore: ${args.datasource.gsName} ${entityType} ${method}`);
+  datastoreSpan = tracer.startSpan(`datastore: ${args.datasource.gsName} ${entityType} ${method}`);
   datastoreSpan.setAttribute('method', method);
   datastoreSpan.setAttribute('model', entityType);
   datastoreSpan.setAttribute('db.system', 'prisma');
 
   // Record metrics to export for datastore
-  const attributes = { hostname: process.env.HOSTNAME, datastore: args.datasource.gsName, model: entityType, method: method };
+  const attributes: PlainObject = { hostname: process.env.HOSTNAME, datastore: args.datasource.gsName, model: entityType, method: method };
   datastoreCounter.add(1, attributes);
   datastoreHistogram.record(Math.random(), attributes);
 
-  if (!prismaMethod) { //Oops!
-    
-    //Check whether the entityType specified is wrong or the method
-    if (!ds.client[entityType]) {
-      datastoreSpan.end();
-      return new GSStatus(false, 400, `Invalid entity type "${entityType}" in query`);
-    }
-    //If not the entity type, the method specified must be wrong.
-    datastoreSpan.end();
-    return new GSStatus(false, 500, `Invalid CRUD method "${entityType}" "${method}" called`);
+  try {
+    prismaMethod = ds.client[entityType][method];
+  } catch (err:any) {
+    if (!prismaMethod) { //Oops!
+      logger.error(err);
+      //Check whether the entityType specified is wrong or the method
+      if (!ds.client[entityType]) {
+        attributes.status_code = 400;
+        status_message = `Invalid entity type "${entityType}" in query`;
+        datastoreSpan.setStatus({ code: SpanStatusCode.ERROR, message: status_message});
+        cleanupTracesMetrics(attributes);
+        return new GSStatus(false, attributes.status_code, undefined, status_message);
+      }
+      //If not the entity type, the method specified must be wrong.
+      attributes.status_code = 500;
+      status_message = `Invalid CRUD method "${entityType}" "${method}" called`;
+      datastoreSpan.setStatus({ code: SpanStatusCode.ERROR, message: status_message});
+      cleanupTracesMetrics(attributes);
+      return new GSStatus(false, attributes.status_code, undefined, status_message);
+    }  
   }
+
   try {
     const res = await prismaMethod.bind(ds.client)(args.data);
-    datastoreSpan.end();
-    return new GSStatus(true, responseCode(method), undefined, res);
+    attributes.status_code = responseCode(method);
+    cleanupTracesMetrics(attributes);
+    return new GSStatus(true, attributes.status_code, undefined, res);
   } catch (err: any) {
     //TODO: better check for error codes. Return 500 for server side error. 40X for client errors.
-    datastoreSpan.end();
-    return new GSStatus(false, 400, err.message || 'Error in query!', JSON.stringify(err.stack));
+    attributes.status_code = 400;
+    status_message = err.message || 'Error in query!';
+    datastoreSpan.setStatus({ code: SpanStatusCode.ERROR, message: status_message});
+    cleanupTracesMetrics(attributes);
+    return new GSStatus(false, attributes.status_code, status_message, JSON.stringify(err.stack));
   }
+
+}
+
+function cleanupTracesMetrics(attributes: PlainObject) {
+  datastoreSpan.setAttribute('status_code', attributes.status_code);
+  datastoreSpan.end();
+  datastoreCounter.add(1, attributes);
+  datastoreHistogram.record(Math.random(), attributes);
 }
 
 function responseCode (method: string): number {
