4c14f5cd002eaf2e15d3c7f876747012ba17283e
Sambasiva Rao Suda <samba@spinbi.com>
2022-12-22 13:26:45 +0530

    importing builtin modules

--- src/scriptRuntime.ts
+++ src/scriptRuntime.ts
@@ -16,6 +16,30 @@ export function importAll(sourceScope: any, targetScope: any) {
     }
 }
 
+import * as fs from 'fs';
+import * as assert from 'assert'
+import * as buffer from 'buffer'
+import * as child_process from 'child_process'
+import * as cluster from 'cluster'
+import * as crypto from 'crypto'
+import * as dgram from 'dgram'
+import * as dns from 'dns'
+import * as events from 'events'
+import * as http from 'http'
+import * as https from 'https'
+import * as net from 'net'
+import * as os from 'os'
+import * as path from 'path'
+import * as querystring from 'querystring'
+import * as readline from 'readline'
+import * as stream from 'stream'
+import * as string_decoder from 'string_decoder'
+import * as timers from 'timers'
+import * as tls from 'tls'
+import * as url from 'url'
+import * as util from 'util'
+import * as zlib from 'zlib'
+
 /**
  * Can be called for gsFunction.args, gsFunction.on_error.transform and switch.condition
  * Input an be scalar or object
@@ -23,7 +47,7 @@ export function importAll(sourceScope: any, targetScope: any) {
 export default async function evaluateScript(ctx: GSContext, script: Function, taskValue?: any) {
 
     logger.debug('before evaluateScript %s', script);
-    
+
     if (!script) {
         return;
     }
