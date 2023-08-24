9ce43d7fefdd2803b3aa49fcdd0b55049aaf4d6e
Ashutosh Tripathi <ashu17706@gmail.com>
2023-08-24 14:40:09 +0530

    deleted unused files, eventually all the plugins will move to a different repo

--- src/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-/*
-* You are allowed to study this software for learning and local * development purposes only. Any other use without explicit permission by Mindgrep, is prohibited.
-* © 2022 Mindgrep Technologies Pvt Ltd
-*/
-export default [];
