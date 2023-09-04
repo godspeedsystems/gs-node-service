d50dd051da4f89ee74bbad0ba8393f98a6dfd00f
Ashutosh Tripathi <ashu17706@gmail.com>
2023-09-04 13:38:56 +0530

    implementations of EventSource and DataSourceAsEventSource

--- src/core/_interfaces/sources.ts
+++ src/core/_interfaces/sources.ts
@@ -1,149 +1,62 @@
 import { PlainObject } from "../../types";
 import { GSCloudEvent, GSContext, GSStatus } from "../interfaces";
 
-
-interface ISource {
+export abstract class GSDataSource {
   config: PlainObject;
 
   client: false | PlainObject;
 
-  init: Function;
-
-  initClient: () => Promise<PlainObject>
-}
-
-interface IDataSource extends ISource {
-  execute: (
-    ctx: GSContext,
-    args: PlainObject,
-  ) => Promise<any>
-}
-
-interface IEventSource extends ISource {
-  subscribeToEvent: (
-    eventKey: string,
-    eventConfig: PlainObject,
-    processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>,
-  ) => Promise<void>
-}
-
-export abstract class Source implements ISource {
-  config: PlainObject;
-
-  client: false | PlainObject;
-
-  constructor(config: PlainObject, client?: PlainObject) {
+  constructor(config: PlainObject) {
     this.config = config;
-    this.client = client || false;
-  }
+    this.client = false;
+  };
 
   async init() {
     this.client = await this.initClient();
   }
 
-  abstract initClient(): Promise<PlainObject>;
-};
+  protected abstract initClient(): Promise<PlainObject>;
 
-export abstract class DataSource extends Source implements IDataSource {
   abstract execute(
     ctx: GSContext,
     args: PlainObject,
   ): Promise<any>
 };
 
-export abstract class DatasourceAsEventSource extends Source implements IEventSource {
-  abstract subscribeToEvent(
-    eventKey: string,
-    eventConfig: PlainObject,
-    processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>,
-  ): Promise<void>
-};
-
-export abstract class EventSource extends DataSource implements IEventSource {
-  abstract subscribeToEvent(
-    eventKey: string,
-    eventConfig: PlainObject,
-    processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>,
-  ): Promise<void>
-}
-// example 1: redis
-class RedisAsDataSource extends DataSource {
-  initClient(): Promise<PlainObject> {
-    throw new Error("Method not implemented.");
-  }
-
-  execute(ctx: GSContext, args: PlainObject): Promise<any> {
-    throw new Error("Method not implemented.");
-  }
-}
-
-class RedisEventSource extends DatasourceAsEventSource {
-  subscribeToEvent(eventKey: string, eventConfig: PlainObject, processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-
-  initClient(): Promise<PlainObject> {
-    throw new Error("Method not implemented.");
-  }
-}
-
-// example 2: express
-class RedisDatasourceAsEventSource extends EventSource {
-  subscribeToEvent(eventKey: string, eventConfig: PlainObject, processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-
-  execute(ctx: GSContext, args: PlainObject): Promise<any> {
-    throw new Error("Method not implemented.");
-  }
-
-  initClient(): Promise<PlainObject> {
-    throw new Error("Method not implemented.");
-  }
-}
-
-
-
-////
-
-export abstract class GSDataSource {
-
+export abstract class GSDataSourceAsEventSource {
   config: PlainObject;
 
-  client?: object;
+  // datasource: PlainObject;
 
-  constructor(
-    config: PlainObject,
-  ) {
+  client: false | PlainObject;
+
+  constructor(config: PlainObject, datasourceClient: PlainObject) {
     this.config = config;
-  }
+    this.client = datasourceClient;
+  };
 
-  async init() {
-    const client = await this.initClient();
-    this.client = client;
-  }
-
-  // Should return client of this datasoure
-  protected abstract initClient(): Promise<object>;
-
-  abstract execute(
-    ctx: GSContext,
-    args: PlainObject,
-  ): Promise<any>
-}
+  abstract subscribeToEvent(
+    eventKey: string,
+    eventConfig: PlainObject,
+    processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>,
+  ): Promise<void>
+};
 
 export abstract class GSEventSource {
   config: PlainObject;
 
-  datasource: GSDataSource;
+  client: false | PlainObject;
 
-  constructor(
-    config: PlainObject,
-    datasource: GSDataSource,
-  ) {
+  constructor(config: PlainObject) {
     this.config = config;
-    this.datasource = datasource;
+    this.client = false;
   };
+
+  public async init() {
+    this.client = await this.initClient();
+  }
+
+  protected abstract initClient(): Promise<PlainObject>;
 
   abstract subscribeToEvent(
     eventKey: string,
