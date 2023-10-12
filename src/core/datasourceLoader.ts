7836164cf2ce94d6b0d8f1b7d45855b8ddfa77a9
Srihari628 <srihari@godspeed.systems>
2023-10-12 12:54:48 +0530

    chore: changed path in glob to work in windows

--- src/core/datasourceLoader.ts
+++ src/core/datasourceLoader.ts
@@ -1,47 +1,56 @@
-import { glob } from "glob";
-import path from "path";
+import { glob } from 'glob';
+import path from 'path';
 
-import { logger } from "../logger";
-import { PlainObject } from "../types";
+import { logger } from '../logger';
+import { PlainObject } from '../types';
 import expandVariables from './expandVariables';
-import loadYaml from "./yamlLoader";
-import { GSDataSource } from "./_interfaces/sources";
-
+import loadYaml from './yamlLoader';
+import { GSDataSource } from './_interfaces/sources';
 
 // we need to scan only the first level of datasources folder
-export default async function (pathString: string): Promise<{ [key: string]: GSDataSource }> {
+export default async function (
+  pathString: string
+): Promise<{ [key: string]: GSDataSource }> {
   let yamlDatasources = await loadYaml(pathString, false);
 
   const prismaDatasources = await loadPrismaDsFileNames(pathString);
   const datasourcesConfigs = { ...yamlDatasources, ...prismaDatasources };
 
   if (datasourcesConfigs && !Object.keys(datasourcesConfigs).length) {
-    throw new Error(`There are no datasources defined in datasource dir: ${pathString}`);
+    throw new Error(
+      `There are no datasources defined in datasource dir: ${pathString}`
+    );
   }
   const datasources: { [key: string]: GSDataSource } = {};
 
   for await (let dsName of Object.keys(datasourcesConfigs)) {
     logger.debug('evaluating datasource %s', dsName);
     datasourcesConfigs[dsName] = expandVariables(datasourcesConfigs[dsName]);
-    logger.debug('evaluated datasource %s %o', dsName, datasourcesConfigs[dsName]);
+    logger.debug(
+      'evaluated datasource %s %o',
+      dsName,
+      datasourcesConfigs[dsName]
+    );
 
     // let's load the loadFn and executeFn
     // there is an assumption that for each datasource, the type's .ts file should be inside /datasources/types folder
     const fileName = datasourcesConfigs[dsName].type;
 
-
-    await import(path.join(pathString, 'types', `${fileName}`))
-      .then(async (Module: GSDataSource) => {
+    await import(path.join(pathString, 'types', `${fileName}`)).then(
+      async (Module: GSDataSource) => {
         const dsYamlConfig: PlainObject = datasourcesConfigs[dsName];
         // @ts-ignore
         const Constructor = Module.default;
         const dsInstance = new Constructor({ ...dsYamlConfig, name: dsName });
         await dsInstance.init(); // This should initialize and set the client in dsInstance
         if (!dsInstance.client) {
-          throw new Error(`Client could not be initialized in your datasource ${dsName}`);
+          throw new Error(
+            `Client could not be initialized in your datasource ${dsName}`
+          );
         }
         datasources[dsName] = dsInstance;
-      });
+      }
+    );
   }
   return datasources;
 }
@@ -50,8 +59,9 @@ async function loadPrismaDsFileNames(pathString: string): Promise<PlainObject> {
   let basePath = path.basename(pathString);
   let prismaSchemas: PlainObject = {};
 
-  const files = glob.sync(pathString + '/**/*.?(prisma)');
-
+  const files = glob.sync(
+    path.join(pathString, '**', '*.?(prisma)').replace(/\\/g, '/')
+  );
   files.forEach((file: string) => {
     const id = file
       .replace(new RegExp(`.*?\/${basePath}\/`), '')
@@ -63,7 +73,7 @@ async function loadPrismaDsFileNames(pathString: string): Promise<PlainObject> {
       ...{
         [id]: {
           type: 'prisma',
-          name: id
+          name: id,
         },
       },
     };
--- src/core/yamlLoader.ts
+++ src/core/yamlLoader.ts
@@ -1,27 +1,36 @@
 /*
-* You are allowed to study this software for learning and local * development purposes only. Any other use without explicit permission by Mindgrep, is prohibited.
-* © 2022 Mindgrep Technologies Pvt Ltd
-*/
+ * You are allowed to study this software for learning and local * development purposes only. Any other use without explicit permission by Mindgrep, is prohibited.
+ * © 2022 Mindgrep Technologies Pvt Ltd
+ */
 import glob from 'glob';
 import yaml from 'yaml';
 import path from 'path';
 
 import { readFileSync } from 'fs';
 
-import { PlainObject } from "./common";
+import { PlainObject } from './common';
 import { logger } from '../logger';
 
-export default function loadYaml(pathString: string, global: boolean = false): PlainObject {
+export default function loadYaml(
+  pathString: string,
+  global: boolean = false
+): PlainObject {
   let basePath = path.basename(pathString);
   let api: PlainObject = {};
-  const files = glob.sync(pathString + '/**/*.?(yaml|yml)');
+  const files = glob.sync(
+    path.join(pathString, '**', '*.?(yaml|yml)').replace(/\\/g, '/')
+  );
   files.map((file: string) => {
     module = yaml.parse(readFileSync(file, { encoding: 'utf-8' }));
-    const id = file.replace(new RegExp(`.*?\/${basePath}\/`), '').replace(/\//g, '.').replace(/\.(yaml|yml)/i, '').replace(/\.index$/, '');
+    const id = file
+      .replace(new RegExp(`.*?\/${basePath}\/`), '')
+      .replace(/\//g, '.')
+      .replace(/\.(yaml|yml)/i, '')
+      .replace(/\.index$/, '');
     if (global) {
       api = {
         ...api,
-        ...module
+        ...module,
       };
     } else {
       if (id == 'index') {
@@ -34,9 +43,6 @@ export default function loadYaml(pathString: string, global: boolean = false): P
 
   return api;
 }
-
-
-
 
 if (require.main === module) {
   (async () => {
