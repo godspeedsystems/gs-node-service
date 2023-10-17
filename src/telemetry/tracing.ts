8885235fd83cf46564026e7e7d21878b30c4ff0d
Ashutosh Tripathi <ashu17706@gmail.com>
2023-10-17 14:34:00 +0530

    fixed tracing for prisma

--- src/telemetry/tracing.ts
+++ src/telemetry/tracing.ts
@@ -2,23 +2,15 @@
 * You are allowed to study this software for learning and local * development purposes only. Any other use without explicit permission by Mindgrep, is prohibited.
 * © 2022 Mindgrep Technologies Pvt Ltd
 */
-import { Span, SpanContext, diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-import { Message } from 'kafkajs';
-import { KafkaJsInstrumentation } from 'opentelemetry-instrumentation-kafkajs';
+import { SpanContext, diag, DiagConsoleLogger } from '@opentelemetry/api';
 import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino';
 import { getEnv } from '@opentelemetry/core';
 import { PrismaInstrumentation } from '@prisma/instrumentation';
 
-const opentelemetry = require("@opentelemetry/sdk-node");
-//Disable all autoinstrumentations because they do logging of all express middleware also.
-//const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
-const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-grpc');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');;
-
-
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';;
 
 let traceExporter;
 
@@ -30,17 +22,10 @@ if (process.env.NODE_ENV != 'dev') {
   process.env.OTEL_TRACES_SAMPLER_ARG = "0.25";
 }
 
-const tracerProvider = new NodeTracerProvider({
-  plugins: {
-    kafkajs: { enabled: false, path: 'opentelemetry-plugin-kafkajs' }
-  }
-});
-
 diag.setLogger(new DiagConsoleLogger(), getEnv().OTEL_LOG_LEVEL);
 
-const sdk = new opentelemetry.NodeSDK({
-  tracerProvider,
-  traceExporter,//: new opentelemetry.tracing.ConsoleSpanExporter(),
+const sdk = new NodeSDK({
+  traceExporter: traceExporter,
   instrumentations: [new HttpInstrumentation({
     requestHook: (span: any, request: any) => {
       if (span.attributes.span_operation === 'INCOMING') {
@@ -65,31 +50,9 @@ const sdk = new opentelemetry.NodeSDK({
       return { span_operation: 'INCOMING' };
     }
   }),
-    ExpressInstrumentation,
-  new KafkaJsInstrumentation({
-    producerHook: (span: Span, topic: string, message: Message) => {
-      span.updateName('Kafka producer: ' + topic);
-
-      const spanCtx: SpanContext = span.spanContext();
-      span.setAttributes({
-        'traceId': spanCtx.traceId,
-        'spanId': spanCtx.spanId
-      });
-    },
-    consumerHook: (span: Span, topic: string, message: Message) => {
-      span.updateName('Kafka consumer: ' + topic);
-
-      const spanCtx: SpanContext = span.spanContext();
-      span.setAttributes({
-        'traceId': spanCtx.traceId,
-        'spanId': spanCtx.spanId
-      });
-    }
-  }),
   new PinoInstrumentation({}),
   new PrismaInstrumentation(),
-  ],
-  ignoreLayers: true
+  ]
 });
 
 //const { logger } = require('../core/logger');
@@ -107,15 +70,5 @@ const sdk = new opentelemetry.NodeSDK({
 
 
 export const initialize = () => {
-  sdk.start()
-    .then(() => console.log('Tracing initialized'))
-    .catch((error: any) => console.error('Error initializing tracing', error));
-
-  // gracefully shut down the SDK on process exit
-  process.on('SIGTERM', () => {
-    sdk.shutdown()
-      .then(() => console.log('Tracing terminated'))
-      .catch((error: any) => console.error('Error terminating tracing', error))
-      .finally(() => process.exit(0));
-  });
+  sdk.start();
 };
