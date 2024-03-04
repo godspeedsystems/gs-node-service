798537ee039e8daca965db4187277773896e5996
mastersilv3r <ayush@godspeed.systems>
2024-03-04 13:00:20 +0530

    fix: swagger.ts replace / with _ in operationId

--- src/router/swagger.ts
+++ src/router/swagger.ts
@@ -38,7 +38,7 @@ export const generateSwaggerJSON = (events: PlainObject, definitions: PlainObjec
       tags: [
         eventSchema.tags
       ],
-      operationId: eventSchema.operationId || eventSchema.id || eventSchema.summary?.replace(' ', '_') || `${method}.${apiEndPoint}`,
+      operationId: eventSchema.operationId || eventSchema.id || eventSchema.summary?.replace(' ', '_') || `${method}.${apiEndPoint}`.replace(/\//g,'_'),
       requestBody: eventSchema.body,
       parameters: eventSchema.params,
       responses: eventSchema.responses,
