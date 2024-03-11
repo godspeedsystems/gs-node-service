05be84ddc380d7714e2e8968ebe5370d00ec4b8d
mastersilv3r <ayush@godspeed.systems>
2024-03-11 09:53:47 +0530

    739_updated_logger.ts to prevent crashing

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -38,8 +38,7 @@ let timestampSetting;
 if (conf.log?.timestamp) {
   timestampSetting = getAtPath(Pino, conf.log.timestamp);
 }
-
-const logger: Pino.Logger = Pino({
+let logger: Pino.Logger = Pino({
   level: conf.log?.level || conf.log_level || 'info',
   //@ts-ignore
   timestamp: timestampSetting,
@@ -70,5 +69,31 @@ const logger: Pino.Logger = Pino({
     censor: '*****'
   }
 });
+
+function loggerFn(logger: any) {
+  ['info', 'debug', 'error', 'fatal'].forEach((val) => {
+    const method = logger[val];
+    // const bound = method.bind(logger);
+    logger[val] = function () {
+      try {
+        method.bind(logger)(...arguments);
+        
+      } catch (e: any) {
+        console.log(`Pino: error executing ${val} {${e.message}}`);
+        console.log(`Printing original error log arguments: %o`, ...arguments);
+      }
+    };
+  });
+  return logger;
+}
+
+logger = loggerFn(logger);
+
+var childFn = logger.child.bind(logger);
+
+logger.child = function (bindings, options) {
+  return loggerFn(childFn(bindings, options));
+};
+
 // process.on('exit', () => {console.log('hesdfasfasfasfasdfasdfasfasdfasdfasdf')});  //logger.flushSync()})
 export { logger };
