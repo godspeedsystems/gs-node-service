6404fa652cb598573f795b1d218701ee84448ddd
Kushal Chauhan <kushal2017328@gmail.com>
2024-06-13 12:05:07 +0530

    feat: Add SSL configuration for Kafka connection

--- src/kafka/index.ts
+++ src/kafka/index.ts
@@ -5,7 +5,7 @@
 import { Consumer, Kafka, Producer, logLevel as kafkaLogLevel }  from 'kafkajs';
 import axios from 'axios';
 import config from 'config';
-
+import fs from 'fs'; 
 import { GSActor, GSCloudEvent, GSStatus } from '../core/interfaces';
 import { logger } from '../core/logger';
 
@@ -196,6 +196,13 @@ export default class KafkaMessageBus {
 
                 return brokers;
               },
+              ssl: {
+                      rejectUnauthorized: false,
+                      key: fs.readFileSync(config.ssl_key),
+                      cert: fs.readFileSync(config.ssl_cert),
+                      ca: [fs.readFileSync(config.ssl_ca)],
+                      passphrase: config.ssl_passphrase
+                    },
               logCreator: pinoLogCreator
         });
 
